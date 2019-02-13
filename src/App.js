39ae1a13ea21db8c7c2a3289db4b16b12254f0b4
James Basile <jbasile14@gmail.com>
2019-02-13 18:04:52 -0500

    init commit, starting to fill out app.js class constructor and handlers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,65 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+const todoItems = [
+  {
+    task: 'Learn User Interface and Git',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Learn Advanced CSS',
+    id: 2,
+    completed: false
+  }
+  {
+    task: 'Learn JavaScript Fundamentals',
+    id: 3,
+    completed: false
+  }
+  {
+    task: 'Learn Applied JavaScript',
+    id: 4,
+    completed: false
+  }
+  {
+    task: 'Complete Project Week: User Interface',
+    id: 5,
+    completed: false
+  }
+  {
+    task: 'Learn React',
+    id: 6,
+    completed: false
+  }
+  {
+    task: 'Learn Intermediate React',
+    id: 7,
+    completed: false
+  }
+]
+
+let lastId = 7;
+const addNewId = () => {
+  lastId++;
+  return lastId;
+};
+
+
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoItems: todoItems,
+      inputText: ''
+    };
+  }
+
+  addtodoItem = todoItem => {
+    
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,29 @@
+import React from 'react';
+
+
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+
+//Component Layout
+// const TodoList = props => {
+//     return (
+//         <div>
+//             {props.*anything*.map( e => e.*something*)}
+//         </div>
+//     )
+// }
+
+
+const TodoList = props => {
+    return (
+        <div className="toDoItem">>
+         
+        
+        </div>
+    );
+};
+
+
+export default TodoList;
